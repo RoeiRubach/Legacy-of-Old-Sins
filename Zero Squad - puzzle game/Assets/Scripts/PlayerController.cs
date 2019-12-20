080b74f4aecb1292156cf809f96091b361f3c939
Roei Rubach <rrubach@gmail.com>
2019-12-20 20:29:02 +0200

    List of added items/objects/etc in the description
    Added a NavMesh to the floor.
    Added a few obstacles.
    Added a character model.
    Added a mouse click movement logic.
    Added a camera movement logic to follow the player.

---
+++ Zero Squad - puzzle game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private NavMeshAgent myAgent;
+
+    [SerializeField]
+    private Camera myCamera;
+
+    [SerializeField]
+    private LayerMask walkableLayerMask;
+
+    private void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit myRacycastHit;
+            Ray myRay = myCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(myRay, out myRacycastHit, Mathf.Infinity, walkableLayerMask))
+                myAgent.SetDestination(myRacycastHit.point);
+        }
+    }
+}
