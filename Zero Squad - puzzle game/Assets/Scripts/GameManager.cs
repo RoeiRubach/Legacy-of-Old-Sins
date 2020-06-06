8ea8fe8eac8d3b84ff0cd9580101ed461af6876a
Roei Rubach <rrubach@gmail.com>
2020-06-06 16:30:56 +0300

    Added another checkpoint after our GameRoom #1

--- Zero Squad - puzzle game/Assets/Scripts/GameManager.cs
+++ Zero Squad - puzzle game/Assets/Scripts/GameManager.cs
@@ -3,5 +3,38 @@
 public class GameManager : SingletonDontDestroy<GameManager>
 {
     public bool IsReachedCheckPoint;
-    public Vector3[] CharactersPlacements;
+    [HideInInspector] public Vector3[] CharactersPlacements;
+    [Range(1,3)]
+    [SerializeField] private int _checkPointToSpawn = 1;
+
+    [SerializeField] private Transform[] _firstCheckPoint;
+    [SerializeField] private Transform[] _secondCheckPoint;
+    [SerializeField] private Transform[] _thirdCheckPoint;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if(IsReachedCheckPoint)
+            switch (_checkPointToSpawn)
+            {
+                case 1:
+                    SetCheckPointsLocation(_firstCheckPoint);
+                    break;
+                case 2:
+                    SetCheckPointsLocation(_secondCheckPoint);
+                    break;
+                case 3:
+                    SetCheckPointsLocation(_thirdCheckPoint);
+                    break;
+            }
+    }
+
+    private void SetCheckPointsLocation(Transform[] placements)
+    {
+        for (int i = 0; i < CharactersPlacements.Length; i++)
+        {
+            CharactersPlacements[i] = placements[i].position;
+        }
+    }
 }
